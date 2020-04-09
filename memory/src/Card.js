01cc0ac71950b4406b2fb75dd8d9d0201b00899c
Cédric R <cedric.ribassin@gmail.com>
2020-04-09 11:58:52 +0200

    Ajout d'évenements et listes de composants

--- memory/src/Card.js
+++ memory/src/Card.js
@@ -4,10 +4,13 @@ import './Card.css'
 
 const HIDDEN_SYMBOL = '❓'
 // card = type du symbole et feedback = état d'affichage de se symbole
-const Card = ({card, feedback}) =>(
-<div className="card">
+ /*si le feedback est un symbol alors on utilise un gros emoji sinon le symbol de la carte*/
+const Card = ({card, feedback, onClick }) =>(
+    // on donne à onclick une fonction qui le moment venu (quand on l'appelle) fera le onclick
+    // si synthaxe onclick={onclick(card) alors pas bon 
+<div className={`card ${feedback}`} onClick={()=> onClick(card)}>
 <span className="symbol">
-    // si le feedbackest un symbol alors on utilise un gros emoji sinon le symbol de la carte
+   
     {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
 </span>
 </div>
--- memory/src/GuessCount.js
+++ memory/src/GuessCount.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import './GuessCount.css'
 
-const GuessCount = () => <div className="guesses" />
+//mise en place du compteur de paires et on destructure la props guesses pour l'afficher
+const GuessCount = ({guesses}) => <div className="guesses">{guesses}</div>
 
 export default GuessCount
